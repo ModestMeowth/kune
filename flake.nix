7e486a4ea24f508028b1b64ecb7cabd888bd4dee
Daniel Eberle <daniel.j.eberle@protonmail.com>
2024-10-18 03:25:54 -0500

    tweaks to skeleton

--- flake.nix
+++ flake.nix
@@ -6,20 +6,59 @@
     devshell.url = "github:numtide/devshell";
     devshell.inputs.nixpkgs.follows = "nixpkgs";
 
-    utils.url = "github:numtide/flake-utils";
+    treefmt.url = "github:numtide/treefmt-nix";
+    treefmt.inputs.nixpkgs.follows = "nixpkgs";
+
+    hooks.url = "github:cachix/git-hooks.nix";
+
+    parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = { self, ... } @ inputs:
-    inputs.utils.lib.eachDefaultSystem (system: {
-      devShells.default = let
-        pkgs = import inputs.nixpkgs {
+  outputs =
+    { ... }@inputs:
+    let
+      genPkgs =
+        system:
+        import inputs.nixpkgs {
           inherit system;
           overlays = builtins.attrValues (import ./overlays.nix { inherit inputs; });
         };
-      in pkgs.devshell.mkShell {
-        imports = [
-          (pkgs.devshell.importTOML ./devshell.toml)
-        ];
-      };
-    });
+    in
+    inputs.parts.lib.mkFlake { inherit inputs; } {
+      systems = inputs.nixpkgs.lib.systems.flakeExposed;
+
+      imports = [
+        inputs.devshell.flakeModule
+        inputs.hooks.flakeModule
+        inputs.treefmt.flakeModule
+      ];
+
+      perSystem =
+        { pkgs, system, ... }:
+        {
+          _module.args.pkgs = genPkgs system;
+
+          treefmt.config = {
+            projectRootFile = "flake.nix";
+
+            programs.nixfmt.enable = true;
+            programs.taplo.enable = true;
+            programs.yamlfmt.enable = true;
+
+            settings.global.excludes = [ "*.sops.*" ];
+          };
+
+          pre-commit.settings.hooks = {
+            treefmt.enable = true;
+            pre-commit-hook-ensure-sops = {
+              enable = true;
+              files = "\.sops\.";
+            };
+          };
+
+          devShells.default = pkgs.devshell.mkShell {
+            imports = [ (pkgs.devshell.importTOML ./devshell.toml) ];
+          };
+        };
+    };
 }
--- overlays.nix
+++ overlays.nix
@@ -1,9 +1,21 @@
-{inputs, ...}: {
+{ inputs, ... }:
+{
   unstable-packages = final: prev: {
     unstable = import inputs.unstable {
       inherit (final) system;
-      overlays = [];
+      overlays = [ ];
     };
   };
+
+  helm-wrapped = final: prev: {
+    kubernetes-helm-wrapped = prev.wrapHelm prev.kubernetes-helm {
+      plugins = with prev.kubernetes-helmPlugins; [
+        helm-diff
+        helm-git
+        helm-unittest
+      ];
+    };
+  };
+
   devshell = inputs.devshell.overlays.default;
 }
