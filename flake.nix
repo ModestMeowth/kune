c37f010cbeb18c2b5ea3cca1b37af4837b06c4f4
Daniel Eberle <daniel.j.eberle@protonmail.com>
2024-10-20 22:06:59 -0500

    formatting and pre-commit

--- flake.nix
+++ flake.nix
@@ -6,11 +6,6 @@
     devshell.url = "github:numtide/devshell";
     devshell.inputs.nixpkgs.follows = "nixpkgs";
 
-    treefmt.url = "github:numtide/treefmt-nix";
-    treefmt.inputs.nixpkgs.follows = "nixpkgs";
-
-    hooks.url = "github:cachix/git-hooks.nix";
-
     parts.url = "github:hercules-ci/flake-parts";
   };
 
@@ -29,33 +24,12 @@
 
       imports = [
         inputs.devshell.flakeModule
-        inputs.hooks.flakeModule
-        inputs.treefmt.flakeModule
       ];
 
       perSystem =
         { pkgs, system, ... }:
         {
           _module.args.pkgs = genPkgs system;
-
-          treefmt.config = {
-            projectRootFile = "flake.nix";
-
-            programs.nixfmt.enable = true;
-            programs.taplo.enable = true;
-            programs.yamlfmt.enable = true;
-
-            settings.global.excludes = [ "*.sops.*" ];
-          };
-
-          pre-commit.settings.hooks = {
-            treefmt.enable = true;
-            pre-commit-hook-ensure-sops = {
-              enable = true;
-              files = "^\w+\.sops\.";
-            };
-          };
-
           devShells.default = pkgs.devshell.mkShell {
             imports = [ (pkgs.devshell.importTOML ./devshell.toml) ];
           };
